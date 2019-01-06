9426b67fdf29b12fc2c9aa32c1b556c87df50fc7
Jaakko Prättälä <iipekaj@gmail.com>
2019-01-06 20:25:50 +0200

    testing

--- nSTest01.py
+++ nSTest01.py
@@ -5,7 +5,7 @@ kick = neuronSeq.NNote(note = 36, velocity = 127, duration = 0.3, channel = 0)
 snare = neuronSeq.NNote(note = 38, velocity = 100, duration = 0.3, channel = 0)
 hihat = neuronSeq.NNote(note = 42, velocity = 83, duration = 0.3, channel = 0)
 
-kick.setNNParams(0.0, 0.000003624, 1.0)
+kick.setNNParams(0.0, 0.000013624, 1.0)
 snare.setNNParams(0.5, 0.00000361213, 1.0)
 hihat.setNNParams(0.1, 0.0000611331, 1.0)
 
@@ -16,8 +16,8 @@ drumConnection02 = neuronSeq.Connection(snare, hihat, -0.0000026,   -0.000002662
 bass01 = neuronSeq.NNote(note = 16, duration = 0.3, velocity = 100, channel = 1)
 bass02 = neuronSeq.NNote(note = 18, duration = 0.3, velocity = 100, channel = 1)
 
-bass01.setNNParams(0.0, 0.0000013, 1.0)
-bass02.setNNParams(0.5, 0.0000013, 1.0)
+bass01.setNNParams(0.0, 0.0000113, 1.0)
+bass02.setNNParams(0.5, 0.0000113, 1.0)
 
 bassConnection00 = neuronSeq.Connection(bass01, bass02, -0.0000001, -0.0000001)
 bassConnection01 = neuronSeq.Connection(bass02, kick, 0.0000001, 0.0000001)
@@ -35,7 +35,7 @@ bassConnection00.start()
 bassConnection01.start()
 bassConnection02.start()
 
-time.sleep(120.0)
+time.sleep(60.0)
 
 drumConnection00.stopSeq()
 drumConnection01.stopSeq()
