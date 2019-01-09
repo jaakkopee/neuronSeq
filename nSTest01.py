e4ca16df17176c6accf497524bd8c24f01ee403a
Jaakko Prättälä <iipekaj@gmail.com>
2019-01-09 20:35:43 +0200

    a bug fix and some testing

--- nSTest01.py
+++ nSTest01.py
@@ -5,37 +5,37 @@ kick = neuronSeq.NNote(note = 36, velocity = 127, duration = 0.3, channel = 0)
 snare = neuronSeq.NNote(note = 38, velocity = 100, duration = 0.3, channel = 0)
 hihat = neuronSeq.NNote(note = 42, velocity = 83, duration = 0.3, channel = 0)
 
-kick.setNNParams(0.0, 0.000013666, 1.0)
-snare.setNNParams(0.5, 0.00000361213, 1.0)
-hihat.setNNParams(0.1, 0.0000611331, 1.0)
+kick.setNNParams(0.99, 0.000000003, 1.0)
+snare.setNNParams(0.6, 0.000003, 1.0)
+hihat.setNNParams(0.1, 0.00003, 1.0)
 
-drumConnection00 = neuronSeq.Connection(kick, snare, -0.0000012631, -0.0000011362)
-drumConnection01 = neuronSeq.Connection(kick, hihat, -0.0000013,    -0.000001326)
-drumConnection02 = neuronSeq.Connection(snare, hihat, -0.0000026,   -0.000002662)
+drumConnection00 = neuronSeq.Connection(kick, snare, -0.0000111, -0.00000111)
+drumConnection01 = neuronSeq.Connection(kick, hihat, -0.00000111, -0.000000000111)
+drumConnection02 = neuronSeq.Connection(snare, hihat,-0.0000000111, -0.0000000111)
 
-bass01 = neuronSeq.NNote(note = 16, duration = 0.3, velocity = 100, channel = 1)
-bass02 = neuronSeq.NNote(note = 18, duration = 0.3, velocity = 100, channel = 1)
+bass01 = neuronSeq.NNote(note = 16, duration = 0.6933, velocity = 100, channel = 1)
+bass02 = neuronSeq.NNote(note = 12, duration = 0.689, velocity = 100, channel = 1)
 
 bass01.setNNParams(0.0, 0.0000113, 1.0)
 bass02.setNNParams(0.5, 0.0000113, 1.0)
 
-bassConnection00 = neuronSeq.Connection(bass01, bass02, -0.0000001, -0.0000001)
-bassConnection01 = neuronSeq.Connection(bass02, kick, 0.0000001, 0.0000001)
-bassConnection02 = neuronSeq.Connection(bass01, kick, 0.00000002, 0.00000002)
+bassConnection00 = neuronSeq.Connection(bass01, bass02, -0.00000222, -0.00000222)
+bassConnection01 = neuronSeq.Connection(bass02, kick, 0.0000000222, 0.0000000222)
+bassConnection02 = neuronSeq.Connection(bass01, kick, 0.00000000222, 0.00000000222)
 
-headOne = neuronSeq.NNote(note = 42, velocity = 100, duration = 0.00001, channel = 2)
-dummyPair = neuronSeq.NNote(note= 42, velocity = 0,duration = 0.00001, channel = 2)
+headOne = neuronSeq.NNote(note = 42, velocity = 0, duration = 0.001, channel = 2)
+dummyPair = neuronSeq.NNote(note= 42, velocity = 0,duration = 0.001, channel = 2)
 
-headOne.setNNParams(0.0, 0.000013666, 1.0)
+headOne.setNNParams(0.0, 0.0000000030303, 1.0)
 dummyPair.setNNParams(0.0, 0.0, 1.0)
 
 headConnection = neuronSeq.Connection(headOne, dummyPair, 0.0, 0.0)
 
-headConnKick = neuronSeq.Connection(headOne, kick, -0.0001, -0.0001)
-headConnSnare = neuronSeq.Connection(headOne, snare, 0.00001, 0.00001)
-headConnHihat = neuronSeq.Connection(headOne, hihat, 0.00001, 0.00001)
-headConnBass01 = neuronSeq.Connection(headOne, bass01, 0.00001, 0.00001)
-HeadConnBass02 = neuronSeq.Connection(headOne, bass02, 0.00001, 0.00001)
+headConnKick = neuronSeq.Connection(headOne, kick, 0.0,  0.000121)
+headConnSnare = neuronSeq.Connection(headOne, snare, 0.0, 0.000121)
+headConnHihat = neuronSeq.Connection(headOne, hihat, 0.0, 0.000121)
+headConnBass01 = neuronSeq.Connection(headOne, bass01, 0.0, 0.000121)
+HeadConnBass02 = neuronSeq.Connection(headOne, bass02, 0.0, 0.000121)
 
 headConnection.start()
 
@@ -53,7 +53,7 @@ bassConnection00.start()
 bassConnection01.start()
 bassConnection02.start()
 
-time.sleep(60.0)
+time.sleep(120.0)
 
 headConnection.stopSeq()
 
--- neuronSeq.py
+++ neuronSeq.py
@@ -23,8 +23,8 @@ class Connection (threading.Thread):
             #Two-way activation:set weights to >0.0,
             #One-way: set either weight to 0.0, other to >0.0
             #free oscillation: set both weights to 0.0
-            self.note[0].activation += self.note[1].activation * self.weight[0] + self.note[0].addToCounter
-            self.note[1].activation += self.note[0].activation * self.weight[1] + self.note[1].addToCounter
+            self.note[0].activation += self.note[1].activation * self.weight[1] + self.note[0].addToCounter
+            self.note[1].activation += self.note[0].activation * self.weight[0] + self.note[1].addToCounter
             
             if self.note[0].activation >= self.note[0].threshold:
                 self.note[0].activation = 0.0
