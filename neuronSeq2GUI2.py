9496fe167646ea8ff0a2b74be073bb77c7f692bb
jaakkopee <iipekaj@gmail.com>
2023-09-08 02:57:55 +0300

    slider creation implemented and buggy

--- neuronSeq2GUI2.py
+++ neuronSeq2GUI2.py
@@ -3,6 +3,7 @@
 # import libraries
 import tkinter as tk
 from tkinter import ttk
+from tkinter import constants as tkConstants
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -12,7 +13,29 @@ import neuronSeq2 as ns2
 import numpy as np
 import networkx as nx
 
+class NSGUISliderWindow(tk.Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.sliders = []
+        return
 
+    def create_widgets(self):
+        #destroy the old widgets
+        for widget in self.winfo_children():
+            widget.destroy()
+
+        #create the sliders
+        for slider in self.sliders:
+            slider.create_widgets()
+
+        self.update()
+        return
+    
+    def add_slider(self, slider):
+        self.sliders.append(slider)
+        return
+    
 # NSGUINerworkCanvas class
 class NSGUINetworkCanvas(FigureCanvasTkAgg):
     def __init__(self, neuron_graph, master=None):
@@ -49,7 +72,194 @@ class NSGUINetworkCanvas(FigureCanvasTkAgg):
         self.draw_neuron_graph()
 
         return
+
+class NSGUIThresholdSlider(tk.Scale):
+    def __init__(self, master=None, ns=None, ci=None, ni=None):
+        super().__init__(master)
+        self.master = master
+        self.ns = ns
+        self.ci = ci
+        self.ni = ni
+        self.pi = ns2.THRESHOLD_PARAMETER
+        return
     
+    def create_widgets(self):
+        self.threshold_slider = tk.Scale(self.master)
+        self.threshold_slider["from_"] = 0
+        self.threshold_slider["to"] = 1
+        self.threshold_slider["resolution"] = 0.01
+        self.threshold_slider["orient"] = tkConstants.VERTICAL
+        self.threshold_slider["length"] = 200
+        self.threshold_slider["label"] = "Threshold "+str(self.ci)+","+str(self.ni)
+        self.threshold_slider["command"] = self.set_threshold
+        column_n = 0
+        for slider in self.master.sliders:
+            column_n += 1
+            if slider.pi==self.pi and slider.ci==self.ci and slider.ni==self.ni:
+                break    
+        self.threshold_slider.grid(row=0, column=column_n, sticky="W")
+        return
+    
+    def set_threshold(self, value):
+        self.ns.set_threshold(self.ci, self.ni, float(value))
+        return
+
+class NSGUIMidiNoteSlider(tk.Scale):
+    def __init__(self, master=None, ns=None, ci=None, ni=None):
+        super().__init__(master)
+        self.master = master
+        self.ns = ns
+        self.ci = ci
+        self.ni = ni
+        self.pi = ns2.MIDI_NOTE_PARAMETER
+        return
+    
+    def create_widgets(self):
+        self.midi_note_slider = tk.Scale(self.master)
+        self.midi_note_slider["from_"] = 0
+        self.midi_note_slider["to"] = 127
+        self.midi_note_slider["resolution"] = 1
+        self.midi_note_slider["orient"] = tkConstants.VERTICAL
+        self.midi_note_slider["length"] = 200
+        self.midi_note_slider["label"] = "MIDI note "+str(self.ci)+","+str(self.ni)
+        self.midi_note_slider["command"] = self.set_midi_note
+        column_n = 0
+        for slider in self.master.sliders:
+            column_n += 1
+            if slider.pi==self.pi and slider.ci==self.ci and slider.ni==self.ni:
+                break    
+        self.midi_note_slider.grid(row=0, column=column_n, sticky="W")
+        return
+    
+    def set_midi_note(self, value):
+        self.ns.set_midi_note(self.ci, self.ni, int(value))
+        return
+
+class NSGUIMidiVelocitySlider(tk.Scale):
+    def __init__(self, master=None, ns=None, ci=None, ni=None):
+        super().__init__(master)
+        self.master = master
+        self.ns = ns
+        self.ci = ci
+        self.ni = ni
+        self.pi = ns2.MIDI_VELOCITY_PARAMETER
+        return
+    
+    def create_widgets(self):
+        self.midi_velocity_slider = tk.Scale(self.master)
+        self.midi_velocity_slider["from_"] = 0
+        self.midi_velocity_slider["to"] = 127
+        self.midi_velocity_slider["resolution"] = 1
+        self.midi_velocity_slider["orient"] = tkConstants.VERTICAL
+        self.midi_velocity_slider["length"] = 200
+        self.midi_velocity_slider["label"] = "MIDI velocity "+str(self.ci)+","+str(self.ni)
+        self.midi_velocity_slider["command"] = self.set_midi_velocity
+        column_n = 0
+        for slider in self.master.sliders:
+            column_n += 1
+            if slider.pi==self.pi and slider.ci==self.ci and slider.ni==self.ni:
+                break    
+        self.midi_velocity_slider.grid(row=0, column=column_n, sticky="W")
+        return
+    
+    def set_midi_velocity(self, value):
+        self.ns.set_midi_velocity(self.ci, self.ni, int(value))
+        return
+    
+class NSGUIMidiDurationSlider(tk.Scale):
+    def __init__(self, master=None, ns=None, ci=None, ni=None):
+        super().__init__(master)
+        self.master = master
+        self.ns = ns
+        self.ci = ci
+        self.ni = ni
+        self.pi = ns2.MIDI_DURATION_PARAMETER
+        return
+    
+    def create_widgets(self):
+        self.midi_duration_slider = tk.Scale(self.master)
+        self.midi_duration_slider["from_"] = 0
+        self.midi_duration_slider["to"] = 10
+        self.midi_duration_slider["resolution"] = 0.1
+        self.midi_duration_slider["orient"] = tkConstants.VERTICAL
+        self.midi_duration_slider["length"] = 200
+        self.midi_duration_slider["label"] = "MIDI duration "+str(self.ci)+","+str(self.ni)
+        self.midi_duration_slider["command"] = self.set_midi_duration
+        column_n = 0
+        for slider in self.master.sliders:
+            column_n += 1
+            if slider.pi==self.pi and slider.ci==self.ci and slider.ni==self.ni:
+                break    
+        self.midi_duration_slider.grid(row=0, column=column_n, sticky="W")
+        return
+    
+    def set_midi_duration(self, value):
+        self.ns.set_midi_duration(self.ci, self.ni, float(value))
+        return
+    
+class NSGUIW0to1Slider(tk.Scale):
+    def __init__(self, master=None, ns=None, ci=None):
+        super().__init__(master)
+        self.master = master
+        self.ns = ns
+        self.ci = ci
+        self.ni = 0
+        self.pi = ns2.WEIGHT_0_1_PARAMETER
+        return
+    
+    def create_widgets(self):
+        self.w0to1_slider = tk.Scale(self.master)
+        self.w0to1_slider["from_"] = -1
+        self.w0to1_slider["to"] = 1
+        self.w0to1_slider["resolution"] = 0.01
+        self.w0to1_slider["orient"] = tkConstants.VERTICAL
+        self.w0to1_slider["length"] = 200
+        self.w0to1_slider["label"] = "W 0 to 1, "+str(self.ci)
+        self.w0to1_slider["command"] = self.set_w0to1
+        column_n = 0
+        for slider in self.master.sliders:
+            column_n += 1
+            if slider.pi==self.pi and slider.ci==self.ci and slider.ni==self.ni:
+                break    
+        self.w0to1_slider.grid(row=0, column=column_n, sticky="W")
+        return
+    
+    def set_w0to1(self, value):
+        self.ns.set_weight_0_to_1(self.ci, float(value))
+        return
+    
+class NSGUIW1to0Slider(tk.Scale):
+    def __init__(self, master=None, ns=None, ci=None):
+        super().__init__(master)
+        self.master = master
+        self.ns = ns
+        self.ci = ci
+        self.ni = 0
+        self.pi = ns2.WEIGHT_1_0_PARAMETER
+        return
+    
+    def create_widgets(self):
+        self.w1to0_slider = tk.Scale(self.master)
+        self.w1to0_slider["from_"] = -1
+        self.w1to0_slider["to"] = 1
+        self.w1to0_slider["resolution"] = 0.01
+        self.w1to0_slider["orient"] = tkConstants.VERTICAL
+        self.w1to0_slider["length"] = 200
+        self.w1to0_slider["label"] = "W 1 to 0, "+str(self.ci)
+        self.w1to0_slider["command"] = self.set_w1to0
+        column_n = 0
+        for slider in self.master.sliders:
+            column_n += 1
+            if slider.pi==self.pi and slider.ci==self.ci and slider.ni==self.ni:
+                break    
+        self.w1to0_slider.grid(row=0, column=column_n, sticky="W")
+        return
+    
+    def set_w1to0(self, value):
+        self.ns.set_weight_1_to_0(self.ci, float(value))
+        return    
+
+ 
 # main window class
 class NeuronSeq2GUI(tk.Tk):
     def __init__(self):
@@ -70,6 +280,10 @@ class NeuronSeq2GUI(tk.Tk):
         self.add_connection_button = tk.Button(self, text="Add Connection", command=self.add_connection)
         self.add_connection_button.grid(row=0, column=1, sticky="W")
 
+        #create add slider button
+        self.add_slider_button = tk.Button(self, text="Add Slider", command=self.add_slider)
+        self.add_slider_button.grid(row=0, column=2, sticky="W")
+
         #create a neuron graph
         self.neuron_graph = ns2.NetworkGraph(self.neuronSeq)
         self.neuron_graph.create_graph()       
@@ -78,6 +292,81 @@ class NeuronSeq2GUI(tk.Tk):
         #create a label for the neuron list output
         self.neuron_list_label = tk.Label(self, text="Neuron List:")
         self.neuron_list_label.grid(row=2, column=0, sticky="W")
+
+        #create slider window
+        self.slider_window = NSGUISliderWindow(self)
+        self.slider_window.title("Sliders")
+        self.slider_window.geometry("400x200")
+        self.slider_window.resizable(width=True, height=False)
+
+
+    def add_slider(self):
+        #create a new window for adding a slider
+        self.add_slider_window = tk.Toplevel(self)
+        self.add_slider_window.title("Add Slider")
+        self.add_slider_window.geometry("400x200")
+        self.add_slider_window.resizable(width=False, height=False)
+    
+        #create a label for the connection index
+        self.slider_connection_index_label = tk.Label(self.add_slider_window, text="Connection Index:")
+        self.slider_connection_index_label.grid(row=0, column=0, sticky="W")
+
+        #create an entry for the connection index
+        self.slider_connection_index_entry = tk.Entry(self.add_slider_window)
+        self.slider_connection_index_entry.grid(row=0, column=1, sticky="W")
+
+        #create a label for the neuron index
+        self.slider_neuron_index_label = tk.Label(self.add_slider_window, text="Neuron Index:")
+        self.slider_neuron_index_label.grid(row=1, column=0, sticky="W")
+
+        #create an entry for the neuron index
+        self.slider_neuron_index_entry = tk.Entry(self.add_slider_window)
+        self.slider_neuron_index_entry.grid(row=1, column=1, sticky="W")
+
+        #create a laber for the parameter index
+        self.slider_parameter_index_label = tk.Label(self.add_slider_window, text="Parameter Index:")
+        self.slider_parameter_index_label.grid(row=2, column=0, sticky="W")
+
+        #create an entry for the parameter index
+        self.slider_parameter_index_entry = tk.Entry(self.add_slider_window)
+        self.slider_parameter_index_entry.grid(row=2, column=1, sticky="W")
+
+        #create a add slider button
+        self.add_slider_button = tk.Button(self.add_slider_window, text="Add Slider", command=self.add_slider_object)
+        self.add_slider_button.grid(row=3, column=0, columnspan=2, sticky="W")
+
+        
+    def add_slider_object(self):
+        #get the slider parameters
+        connection_index = int(self.slider_connection_index_entry.get())
+        neuron_index = int(self.slider_neuron_index_entry.get())
+        parameter_index = int(self.slider_parameter_index_entry.get())
+
+        #create slider
+        if parameter_index==ns2.THRESHOLD_PARAMETER:
+            slider = NSGUIThresholdSlider(self.slider_window, self.neuronSeq, connection_index, neuron_index)
+        elif parameter_index==ns2.MIDI_NOTE_PARAMETER:
+            slider = NSGUIMidiNoteSlider(self.slider_window, self.neuronSeq, connection_index, neuron_index)
+        elif parameter_index==ns2.MIDI_VELOCITY_PARAMETER:
+            slider = NSGUIMidiVelocitySlider(self.slider_window, self.neuronSeq, connection_index, neuron_index)
+        elif parameter_index==ns2.MIDI_DURATION_PARAMETER:
+            slider = NSGUIMidiDurationSlider(self.slider_window, self.neuronSeq, connection_index, neuron_index)
+        elif parameter_index==ns2.WEIGHT_0_1_PARAMETER:
+            slider = NSGUIW0to1Slider(self.slider_window, self.neuronSeq, connection_index)
+        elif parameter_index==ns2.WEIGHT_1_0_PARAMETER:
+            slider = NSGUIW1to0Slider(self.slider_window, self.neuronSeq, connection_index)
+        else:
+            print("Error: invalid parameter index")
+
+        #add the slider to the slider window
+        self.slider_window.add_slider(slider)
+
+        #create the slider widgets
+        self.slider_window.create_widgets()
+
+
+        #destroy the add slider window
+        self.add_slider_window.destroy()
 
     def add_neuron_note(self):
         #create a new window for adding a neuron/note
